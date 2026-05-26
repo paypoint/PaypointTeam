8d3878f3bf3d3a97cfb19ce1009a429a0f63b3e1
Shreya.Raut <Shreya.Raut@UTPL-62>
2026-05-26 10:53:41 +0530

    add new react features

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "SaaS Team",
+  title: "Paypoint Team",
   tagline: "Training Documentation",
   favicon: "img/favicon.ico",
 
